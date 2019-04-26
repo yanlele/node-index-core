8458be51cd0504fb2f25c824aabaa992d0cb9b37
yanlele <331393627@qq.com>
2019-04-26 22:14:18 +0800

    feat(设计模式): 抽象工厂与抽象实现

---
+++ src/book/04、设计模式/02、创建设计模式/05、抽象工厂/抽象与实现.ts
@@ -0,0 +1,40 @@
+class Car {
+  private type: string;
+  constructor() {
+    this.type = 'car'
+  }
+  getPrice(): Error | number{
+    return new Error('匿名类不可调用');
+  }
+
+  getSpeed(): Error | number {
+    return new Error('匿名类不可调用');
+  }
+}
+
+class BMW extends Car {
+  private readonly price: number;
+  private readonly speed: number;
+
+  constructor(price, speed) {
+    super();
+    this.price = price;
+    this.speed = speed;
+  }
+
+  getPrice(): Error | number {
+    return this.price;
+  }
+
+  getSpeed(): Error | number {
+    return this.speed;
+  }
+}
+
+const bmw = new BMW(10000, 200);
+console.log('price', bmw.getPrice());
+console.log('speed', bmw.getSpeed());
+
+
+
+
