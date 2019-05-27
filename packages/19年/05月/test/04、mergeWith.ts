7d4920308ee7692a14e1b39501da98d24a527401
yanlele <331393627@qq.com>
2019-05-27 20:42:05 +0800

    feat(19年): add demo for mergeWith

---
+++ packages/19年/05月/test/04、mergeWith.ts
@@ -0,0 +1,35 @@
+import { mergeWith } from 'lodash';
+
+// function customizer(objValue, srcValue) {
+//   if (isArray(objValue)) {
+//     console.log(`<${'='.repeat(100)}>`);
+//     console.log(objValue);
+//     console.log(srcValue);
+//     console.log(`<${'='.repeat(100)}>`);
+//     return objValue.concat(srcValue);
+//   }
+// }
+
+const object = { a: [1], b: [2], c: 5 };
+const other = { a: [3], b: [4] };
+
+const merge = (...items) => {
+  const args = [
+    ...items,
+    function(objValue, srcValue) {
+      console.log(`<${'='.repeat(100)}>`);
+      console.log(objValue);
+      console.log(srcValue);
+      console.log(`<${'='.repeat(100)}>`);
+      return function(...options) {
+        console.log(options);
+        console.log(this);
+        objValue.concat(srcValue);
+      };
+    },
+  ];
+
+  return mergeWith.apply(undefined, args);
+};
+
+console.log(merge(object, other));
