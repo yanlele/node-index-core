dbc381884519736201f051fde24ef6bc61e34846
yanlele <331393627@qq.com>
2019-10-07 12:20:19 +0800

    添加一个dmeo

---
+++ packages/19年/10月/test/01、async&await.ts
@@ -0,0 +1,21 @@
+const func1 = () => new Promise(resolve => resolve('func1'));
+
+let timer = 0;
+const func = async () => {
+  timer++;
+  if (timer >= 10) {
+    return 'loop'
+  }
+
+  return await func();
+};
+
+const main = async () => {
+  const res = await func();
+
+  console.log(res);
+};
+
+main();
+
+export default {};
