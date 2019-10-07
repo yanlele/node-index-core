ff1553d43f0479bb76c775d8a2d971450f76f212
yanlele <331393627@qq.com>
2019-10-07 15:00:55 +0800

    catch error

--- packages/spider/src/workDemo/downloadMV/getDetailInfo.ts
+++ packages/spider/src/workDemo/downloadMV/getDetailInfo.ts
@@ -30,34 +30,35 @@ export const getDetailInfo = async (page: Page) => {
 
   if (isEmpty(info)) return;
 
-  await page.goto(info.detail_url);
-  await page.waitFor(500);
-
-
-  await page.waitForSelector('#ct');
-  const htmlString: string = await page.evaluate(() => document.body.innerHTML);
-
-  const $: CheerioStatic = load(htmlString);
-
-  const lookOver = $('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(2)').text();
-  const reply = $('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(5)').text();
-
-
-  const a = $('a').filter((index, element) => {
-    const href = $(element).attr('href');
-    return isString(href) && (!href.includes('&nothumb=yes') && href.includes('forum.php?mod=attachment&aid='))
-  });
-  const downloadUrl = `${host}${a.attr('href')}`;
-
-  console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
-  console.log('info: ', info);
-  console.log('阅读量 - lookOver： ', lookOver);
-  console.log('回复量 - reply： ', reply);
-  console.log('下载链接： ', downloadUrl);
-  console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
-  console.log();
-
   try {
+    await page.goto(info.detail_url);
+    await page.waitFor(500);
+
+
+    await page.waitForSelector('#ct');
+    const htmlString: string = await page.evaluate(() => document.body.innerHTML);
+
+    const $: CheerioStatic = load(htmlString);
+
+    const lookOver = $('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(2)').text();
+    const reply = $('#postlist > table:nth-child(1) > tbody > tr > td.pls.ptn.pbn > div > span:nth-child(5)').text();
+
+
+    const a = $('a').filter((index, element) => {
+      const href = $(element).attr('href');
+      return isString(href) && (!href.includes('&nothumb=yes') && href.includes('forum.php?mod=attachment&aid='))
+    });
+    const downloadUrl = `${host}${a.attr('href')}`;
+
+    console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
+    console.log('info: ', info);
+    console.log('阅读量 - lookOver： ', lookOver);
+    console.log('回复量 - reply： ', reply);
+    console.log('下载链接： ', downloadUrl);
+    console.log(`<${'='.repeat(50)}${'='.repeat(50)}>`);
+    console.log();
+
+
     await query(
       `update store set look_over = ?, reply = ?, download_url = ? where id = ?`,
       [lookOver, reply, downloadUrl, info.id],
