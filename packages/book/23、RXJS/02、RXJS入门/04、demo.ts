864266934f01cb34e8dabc537ce66b06583a95ae
yanlele <331393627@qq.com>
2019-11-08 23:06:13 +0800

    feat(book): add Observable的结束

--- packages/book/23、RXJS/02、RXJS入门/04、demo.ts
+++ packages/book/23、RXJS/02、RXJS入门/04、demo.ts
@@ -1,22 +1,32 @@
+// 如果Observable 是不间断的推送出一串正整数
+// 需要在最后结束的时候调用终结程序的方法
+
+// RxJS v6+
 import { Observable } from 'rxjs';
+import { Subscriber } from 'rxjs/src/internal/Subscriber';
+import { PartialObserver, TeardownLogic } from 'rxjs/src/internal/types';
 
 const { create } = Observable;
 
-const hello$: Observable<string> = create(observer => {
-  observer.next('Hello');
-  observer.next('world');
-  // observer.error('error');
-  observer.complete();
-});
+type OnSubscribe<T> = (subscriber: Subscriber<T>) => TeardownLogic;
 
-hello$.subscribe({
-  next: val => {
-    console.log(val);
-  },
-  error: value => {
-    console.log('error: ', value);
-  },
-  complete: () => {
-    console.log('complete: ');
-  },
-});
+const onSubscribe: OnSubscribe<number> = observer => {
+  let time = 0;
+  const handleInterval = setInterval(() => {
+    time += 1;
+    observer.next(time);
+    if (time > 5) {
+      clearInterval(handleInterval);
+      observer.complete();
+    }
+  }, 500);
+};
+
+const source$: Observable<number> = create(onSubscribe);
+
+const theObserver: PartialObserver<number> = {
+  next: item => console.log(item),
+  complete: () => console.log('没有更多数据'),
+};
+
+source$.subscribe(theObserver);
