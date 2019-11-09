1531450c40ebdf6d33264592482ed6128cd1eaea
yanlele <331393627@qq.com>
2019-11-09 14:10:14 +0800

    feat: 下⾯的代码⽰例通过pipe串接了filter和map两个lettable操作符

---
+++ packages/book/23、RXJS/03、操作符基础/02、demo.ts
@@ -0,0 +1,14 @@
+/**
+ * 下⾯的代码⽰例通过pipe串接了filter和map两个lettable操作符：
+ */
+import { of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+
+const source$ = of(1, 2, 3, 4, 5);
+const resouce$ = source$.pipe(
+  filter(x => x % 2 === 0),
+  map(x => x * 2),
+);
+
+resouce$.subscribe(console.log);
