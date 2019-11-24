610beb91665d9a3a48fcb4db4778f43ee226166a
yanlele <331393627@qq.com>
2019-11-24 20:23:37 +0800

    feat: 其他demo

--- packages/book/23、RXJS/05、合并数据流/14、combineLatest - 组合3个定时发送.ts
+++ packages/book/23、RXJS/05、合并数据流/14、combineLatest - 组合3个定时发送.ts
@@ -1,4 +1,17 @@
-/**
- * create by yanle
- * create time 2019-11-24 20:11
- */
+import { combineLatest, timer } from 'rxjs';
+
+const timerOne$ = timer(1000, 4000);
+const timerTwo$ = timer(2000, 4000);
+const timerThree$ = timer(3000, 4000);
+
+const combined$ = combineLatest(timerOne$, timerTwo$, timerThree$);
+
+combined$.subscribe(latestValues => {
+  const [one, two, three] = latestValues;
+
+  console.log(`
+    timer one latest: ${one};
+    timer two latest: ${two};
+    timer three latest: ${three};
+  `);
+});
