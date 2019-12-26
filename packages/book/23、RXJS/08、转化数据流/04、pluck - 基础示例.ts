5cdcd0266798d0e49b06f2bc1b2ee172d12d4cc7
yanlele <331393627@qq.com>
2019-12-26 23:58:59 +0800

    feat: pluck - 基础示例

---
+++ packages/book/23、RXJS/08、转化数据流/04、pluck - 基础示例.ts
@@ -0,0 +1,15 @@
+import { of } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+const source$ = of(
+  { name: 'RxJS', version: 'v4' },
+  { name: 'React', version: 'v15' },
+  { name: 'React', version: 'v16' },
+  { name: 'RxJS', version: 'v5' },
+);
+
+const result$ = source$.pipe(
+  pluck('name'),
+);
+
+result$.subscribe(value => console.log(value));
