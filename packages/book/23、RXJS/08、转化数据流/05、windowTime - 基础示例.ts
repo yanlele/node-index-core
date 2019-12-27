777298904e75953b22ccd614165323789fab459d
yanlele <331393627@qq.com>
2019-12-27 23:48:11 +0800

    feat: 每个指定持续时间都会开启新窗

--- packages/book/23、RXJS/08、转化数据流/05、windowTime - 基础示例.ts
+++ packages/book/23、RXJS/08、转化数据流/05、windowTime - 基础示例.ts
@@ -1,8 +1,11 @@
 import { timer } from 'rxjs';
-import { windowTime } from 'rxjs/operators';
+import { mergeAll, windowTime } from 'rxjs/operators';
 
 
 const source$ = timer(0, 100);
-const result$ = source$.pipe(windowTime(400));
+const result$ = source$.pipe(
+  windowTime(400),
+  mergeAll()
+);
 
 result$.subscribe(value => console.log(value));
---
+++ packages/book/23、RXJS/08、转化数据流/06、windowTime - 每个指定持续时间都会开启新窗⼝.ts
@@ -0,0 +1,28 @@
+import { timer } from 'rxjs';
+import { map, mergeAll, take, tap, windowTime } from 'rxjs/operators';
+
+const source$ = timer(0, 1000);
+const example$ = source$.pipe(
+  windowTime(3000),
+  tap(()=> console.log('new window')),
+  mergeAll()
+);
+
+example$.subscribe(value => console.log(value));
+
+/*
+结果：
+new window
+0
+1
+2
+new window
+3
+4
+5
+new window
+6
+7
+8
+.......
+* */
