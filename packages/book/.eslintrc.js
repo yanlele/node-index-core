723b37ea220828ced73129c78e42830842dd390f
yanlele <331393627@qq.com>
2020-01-08 21:35:05 +0800

    feat: add eslintrc.js

---
+++ packages/book/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': [2, { functions: false }],
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        varsIgnorePattern: 'React|wiredRegeneratorRuntimePolyfill',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      2,
+      {
+        allowExpressions: true,
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/explicit-member-accessibility': 2,
+    camelcase: 0,
+    '@typescript-eslint/camelcase': [
+      1,
+      {
+        allow: [],
+      },
+    ],
+  },
+};
