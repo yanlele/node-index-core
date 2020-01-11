d74dbbf268dfac6eeeb831c142f69fd6dfce9e63
yanlele <331393627@qq.com>
2020-01-11 22:13:16 +0800

    feat(base-19年): modify listPackages

--- utils/listPackages.js
+++ utils/listPackages.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const glob = require('glob');
+const { flattenDeep } = require('lodash');
 
 const prefix = path.resolve(__dirname, '../packages/');
-const packages = glob.sync(`${prefix}/*`).map(package => path.basename(package));
+const packages = glob.sync(`${prefix}/*`).map(packagePath => {
+  if (path.basename(packagePath) === 'base')
+    return glob
+      .sync(`${packagePath}/*`)
+      .filter(item => !path.basename(item).includes('.'))
+      .map(item => `base-${path.basename(item)}`);
+  return path.basename(packagePath);
+});
 
 packages.push('build');
-exports.packages = packages;
+exports.packages = flattenDeep(packages);
 
 exports.prefix = prefix;
