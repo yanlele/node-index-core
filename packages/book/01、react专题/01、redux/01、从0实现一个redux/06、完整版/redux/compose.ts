7d6e60960aa7a6df7c551a146301843832798224
yanlele <331393627@qq.com>
2020-01-23 23:12:40 +0800

    feat(book-01、react专题): redux - compose

---
+++ packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/compose.ts
@@ -0,0 +1,9 @@
+const componse = (...funcs: Function[]) => {
+  if (funcs.length === 0) return (arg: any) => arg;
+
+  if (funcs.length === 1) return funcs[0];
+
+  return funcs.reduce((a, b) => (...args: any[]) => a(b(...args)));
+};
+
+export default componse;
