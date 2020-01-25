ef9c9803473126a88e51e43658790644247f4a65
yanlele <331393627@qq.com>
2020-01-25 23:46:33 +0800

    feat(book-01、react专题): bindActionCteators

--- packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/bindActionCreator.ts
+++ packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/bindActionCreator.ts
@@ -2,3 +2,19 @@ import { Dispatch } from '../../redux';
 
 const bindActionCreator = (actionCreator: Function, dispatch: Dispatch) => (...args: any) =>
   dispatch(actionCreator.apply(this, args));
+
+const bindActionCteators = (actioncreators: Function | { [key: string]: Function }, dispatch: Dispatch) => {
+  if (typeof actioncreators === 'function') return bindActionCreator(actioncreators, dispatch);
+
+  const keys: string[] = Object.keys(actioncreators);
+  const boundActionCreators: { [key: string]: Function } = {};
+
+  keys.forEach(key => {
+    const actionCreator = actioncreators[key];
+    boundActionCreators[key] = bindActionCreator(actionCreator, dispatch);
+  });
+
+  return boundActionCreators;
+};
+
+export default bindActionCteators;
