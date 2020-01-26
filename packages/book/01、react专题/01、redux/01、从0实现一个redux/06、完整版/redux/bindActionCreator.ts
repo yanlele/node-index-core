4a2156ca91b0a38187d900a8fdf46946a5b06ea8
yanlele <331393627@qq.com>
2020-01-26 21:48:45 +0800

    feat(book-01、react专题): file rename

--- packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/bindActionCreator.ts
+++ packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/bindActionCreator.ts
@@ -3,18 +3,18 @@ import { Dispatch } from '../../redux';
 const bindActionCreator = (actionCreator: Function, dispatch: Dispatch) => (...args: any) =>
   dispatch(actionCreator.apply(this, args));
 
-const bindActionCteators = (actioncreators: Function | { [key: string]: Function }, dispatch: Dispatch) => {
-  if (typeof actioncreators === 'function') return bindActionCreator(actioncreators, dispatch);
+const bindActionCreators = (actionCreators: Function | { [key: string]: Function }, dispatch: Dispatch) => {
+  if (typeof actionCreators === 'function') return bindActionCreator(actionCreators, dispatch);
 
-  const keys: string[] = Object.keys(actioncreators);
+  const keys: string[] = Object.keys(actionCreators);
   const boundActionCreators: { [key: string]: Function } = {};
 
   keys.forEach(key => {
-    const actionCreator = actioncreators[key];
+    const actionCreator = actionCreators[key];
     boundActionCreators[key] = bindActionCreator(actionCreator, dispatch);
   });
 
   return boundActionCreators;
 };
 
-export default bindActionCteators;
+export default bindActionCreators;
