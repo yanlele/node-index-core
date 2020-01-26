897961f9a706e62ba3154913f4ba8443a543db23
yanlele <331393627@qq.com>
2020-01-26 21:45:16 +0800

    feat(book-01、react专题): bind reducer

---
+++ packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/createStore.ts
@@ -0,0 +1,62 @@
+import { Reducer } from '../../redux';
+
+type Subscribe = (listener: Function) => void;
+export type Dispatch<T = any> = (action: Action) => void;
+type GetState<T> = () => T;
+type ReplaceReducer = (nextReducer: Reducer) => void;
+
+export interface Action {
+  type: string | symbol;
+
+  [key: string]: any;
+}
+
+type Middleware = (createStore: CreateStore) => CreateStore;
+
+export interface Store<T = any> {
+  subscribe: Subscribe;
+  dispatch: Dispatch<T>;
+  getState: GetState<T>;
+  replaceReducer: ReplaceReducer;
+}
+
+export type CreateStore = <T>(reducer: Reducer<T>, initState?: T, middleware?: Middleware) => Store<T>;
+
+const createStore: CreateStore = <T>(reducer: Reducer<T>, initState: T, middleware: Middleware) => {
+  let state = initState;
+  const listeners: Function[] = [];
+
+  if (middleware) {
+    const newCreateStore = middleware(createStore);
+    return newCreateStore(reducer, initState);
+  }
+
+  const subscribe: Subscribe = listener => {
+    listeners.push(listener);
+  };
+
+  const dispatch: Dispatch<T> = action => {
+    state = reducer(initState, action);
+    listeners.forEach(listener => {
+      listener();
+    });
+  };
+
+  const replaceReducer: ReplaceReducer = (nextReducer: Reducer) => {
+    reducer = nextReducer;
+    dispatch({ type: Symbol() });
+  };
+
+  dispatch({ type: Symbol() });
+
+  const getState: GetState<T> = () => state;
+
+  return {
+    subscribe,
+    dispatch,
+    getState,
+    replaceReducer,
+  };
+};
+
+export default createStore;
---
+++ packages/book/01、react专题/01、redux/01、从0实现一个redux/06、完整版/redux/index.ts
@@ -0,0 +1,7 @@
+import createStore from './createStore';
+import combineReducers from './combineReducers';
+import applyMiddleware from './applyMiddleware';
+import bindActionCreators from './bindActionCreator';
+import compose from './compose';
+
+export { createStore, combineReducers, applyMiddleware, bindActionCreators, compose };
