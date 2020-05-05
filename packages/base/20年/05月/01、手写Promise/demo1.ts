6f66c9f3d6c26cc70e3464c776ad25de05f7f1e8
yanlele <331393627@qq.com>
2020-05-05 14:53:13 +0800

    feat: 手写promise

---
+++ packages/base/20年/05月/01、手写Promise/demo1.ts
@@ -0,0 +1,44 @@
+/**
+ * 我们知道，在创建一个Promise实例时，都会立即执行executor函数，
+ * executor函数传递两个参数，resolve和reject，如果executor函数执行错误，Promise实例状态会变为rejected
+ */
+class MyPromise {
+  private status: 'pending' | 'resolved' | 'rejected';
+  private value: any;
+
+  constructor(executor: Function) {
+    this.status = 'pending';
+    this.value = undefined;
+
+    const resolve = (result: any) => {
+      if (this.status !== 'pending') return;
+      this.status = 'resolved';
+      this.value = result;
+    };
+
+    const reject = (response: any) => {
+      if (this.status !== 'pending') return;
+      this.status = 'rejected';
+      this.value = response;
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
+  }
+}
+
+/* ==============================  测试 - Start ============================== */
+const p1 = new MyPromise((resolve: any, reject: any) => {
+  resolve(1);
+});
+
+const p2 = new MyPromise((resolve: any, reject: any) => {
+  reject(2);
+});
+
+console.log(p1);
+console.log(p2);
+/* ==============================  测试 - End   ============================== */
