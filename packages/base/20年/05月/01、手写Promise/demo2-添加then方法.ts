124d08b3f7a889cfbb14e0f688a0e778b7c9cf20
yanlele <331393627@qq.com>
2020-06-16 23:55:38 +0800

    feat: 测试

--- packages/base/20年/05月/01、手写Promise/demo2-添加then方法.ts
+++ packages/base/20年/05月/01、手写Promise/demo2-添加then方法.ts
@@ -7,7 +7,7 @@ class MyPromise {
   resolveArr: Function[];
   rejectArr: Function[];
 
-  constructor(executor: Function) {
+  constructor(executor: (resolve: Function, reject: Function) => void) {
     this.status = 'pending'; // 初始化状态为pending
     this.value = undefined; // 初始化返回的成功的结果或者失败的原因
     this.resolveArr = []; // 初始化then中成功的方法
@@ -28,10 +28,28 @@ class MyPromise {
       });
     };
     // 这里是resolve方法，成功后执行，将状态改变为resolved，并且将结果返回
-    const resolve = (resolve: any): void => change('resolved', resolve);
+    const resolve = (result: any): void => {
+      // 如果有值， 立即改变状态
+      if (this.resolveArr.length > 0) {
+        change('resolved', result);
+      }
+      // 没有值，延后改变状态
+      const timer = setTimeout(() => {
+        change('resolved', result);
+        clearTimeout(timer);
+      });
+    };
 
     // 这里是reject方法，异常时执行，状态改为rejected，并且将失败的原因返回
-    const reject = (reject: any): void => change('rejected', reject);
+    const reject = (reason: any): void => {
+      if (this.rejectArr.length > 0) {
+        change('rejected', reason);
+      }
+      const timer = setTimeout(() => {
+        change('rejected', reason);
+        clearTimeout(timer);
+      });
+    };
 
     // try、catch捕获异常，如果错误，执行reject方法
     try {
@@ -91,16 +109,50 @@ export default {};
 // );
 
 // 做一点小小的改动
-new MyPromise((resolve: Function) => {
-  setTimeout(() => {
-    resolve(1);
-  }, 0);
+new MyPromise((resolve: Function, reject: Function) => {
+  resolve('我成功啦，吼吼吼～～～～');
+  reject('我都已经成功了，你别想让我失败，哼～～');
 }).then(
-  (res: number) => {
+  (res: string) => {
     console.log(res, 'success');
   },
-  (err: number) => {
+  (err: string) => {
     console.log(err, 'error');
   },
 );
+
+new MyPromise((resolve, reject) => {
+  reject('失败了，我好委屈，呜呜呜～～');
+  resolve('已经失败了～～～');
+}).then(
+  (res: string) => {
+    console.log(res, 'success');
+  },
+  (err: string) => {
+    console.log(err, 'error');
+  },
+);
+
+// 3、链式调用
+new MyPromise((resolve, reject) => {
+  reject('失败了，我好委屈，呜呜呜～～');
+  resolve('已经失败了～～～');
+})
+  .then(
+    (res: string) => {
+      console.log(res);
+    },
+    (err: string) => {
+      console.log(err, 'error'); // 失败了，我好委屈，呜呜呜～～ error
+      return '我要发奋图强，不会被困难所击倒，我要成功！！！';
+    },
+  )
+  .then(
+    (res1: string) => {
+      console.log(res1, '经过不懈努力，我终于在第二次成功了～'); // 我要发奋图强，不会被困难所击倒，我要成功！！！  经过不懈努力，我终于在第二次成功了～
+    },
+    (err1: string) => {
+      console.log(err1, '第二次失败');
+    },
+  );
 /* ==============================  测试 - End   ============================== */
