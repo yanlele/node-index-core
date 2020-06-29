cd7dcbce253f687d7f5fb4b2b54197082f0b88c8
yanlele <331393627@qq.com>
2020-06-29 23:30:44 +0800

    feat: 添加demo

---
+++ packages/base/20年/06月/01、理解ES6的 Iterator 、Iterable 、 Generator/01、IterableDemo.ts
@@ -0,0 +1,21 @@
+const createIterator = (items: any[]) => {
+  let i = 0;
+  return {
+    next: () => {
+      const done = i >= items.length;
+      const value = !done ? items[i++] : undefined;
+      return {
+        done,
+        value,
+      };
+    },
+  };
+};
+
+const iterator = createIterator([1, 3, 5]);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+console.log(iterator.next());
