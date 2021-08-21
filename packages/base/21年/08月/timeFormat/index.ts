16682f763271e61f729e3ac7a6efbf6eb297abe5
yanlele <331393627@qq.com>
2021-08-21 17:52:09 +0800

    feat: 日期格式化

--- packages/base/21年/08月/timeFormat/index.ts
+++ packages/base/21年/08月/timeFormat/index.ts
@@ -1,7 +1,7 @@
-const MONTH = ['', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
-const WEEK = ['日', '一', '二', '三', '四', '五', '六'];
+const MONTH_DISPLAY = ['', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+const WEEK_DISPLAY = ['日', '一', '二', '三', '四', '五', '六'];
 // 单位大小
-const UNIT = {
+const UNIT_SIZE = {
   d: 86400000, // 毫秒/天
   H: 3600000, // 毫秒/小时
   m: 60000, // 毫秒/分钟
@@ -9,11 +9,22 @@ const UNIT = {
   S: 1, // 毫秒
 };
 
-const timeFormat = () => {
-  console.log(MONTH);
-  console.log(WEEK);
-  console.log(UNIT);
-  console.log('123');
-};
+const timeFormat = (timeInput?: string) => {
+  const timeHelper = (date: Date) => {
+    const fillZero = (num: number) => (num < 10 ? '0' + num : num);
+    const YYYY = () => date.getFullYear(); // 年
+    const M = () => date.getMonth() + 1;
+    const Q = () => Math.ceil(M() / 3); // 季度
+    const MM = () => fillZero(M());
+    const MMM = () => M() + '月'; // 月份 [1月..12月]
+    const MMMM = () => MONTH_DISPLAY[M()]; // 月份 [一月..十二月]
+    const D = () => date.getDate();
+    const Dd = () => D() + '日';
+    const DD = () => fillZero(D());
+  };
 
-timeFormat();
+  const format = (formatString: string) => {
+    const reg = /Y{4}|M{1,4}|Do|D{1,2}|d{1,4}|Q|H{1,2}|h{1,2}|m{1,2}|s{1,2}|S{1,3}|A|a|x|X/g;
+    return formatString.replace(reg, match => match);
+  };
+};
