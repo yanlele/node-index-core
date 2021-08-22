8d2c3c3085d301f789f450aaa6f0e1e487572bcc
yanlele <331393627@qq.com>
2021-08-22 11:50:20 +0800

    feat: 添加处理时间的方法

--- packages/base/21年/08月/timeFormat/index.ts
+++ packages/base/21年/08月/timeFormat/index.ts
@@ -1,7 +1,7 @@
-const MONTH_DISPLAY = ['', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
-const WEEK_DISPLAY = ['日', '一', '二', '三', '四', '五', '六'];
+const MONTH = ['', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
+const WEEK = ['日', '一', '二', '三', '四', '五', '六'];
 // 单位大小
-const UNIT_SIZE = {
+const UNIT = {
   d: 86400000, // 毫秒/天
   H: 3600000, // 毫秒/小时
   m: 60000, // 毫秒/分钟
@@ -9,18 +9,34 @@ const UNIT_SIZE = {
   S: 1, // 毫秒
 };
 
-const timeFormat = (timeInput?: string) => {
-  const timeHelper = (date: Date) => {
+const timeFormat = (timeInput?: string | Date) => {
+  const date = timeInput ? new Date(timeInput) : new Date();
+  const timeHelper = () => {
     const fillZero = (num: number) => (num < 10 ? '0' + num : num);
     const YYYY = () => date.getFullYear(); // 年
     const M = () => date.getMonth() + 1;
     const Q = () => Math.ceil(M() / 3); // 季度
     const MM = () => fillZero(M());
     const MMM = () => M() + '月'; // 月份 [1月..12月]
-    const MMMM = () => MONTH_DISPLAY[M()]; // 月份 [一月..十二月]
+    const MMMM = () => MONTH[M()]; // 月份 [一月..十二月]
     const D = () => date.getDate();
     const Dd = () => D() + '日';
     const DD = () => fillZero(D());
+    const d = () => date.getDay();
+    const dd = () => WEEK[d()];
+    const ddd = () => '周' + dd();
+    const dddd = () => '星期' + dd();
+    const H = () => date.getHours();
+    const HH = () => fillZero(H());
+    const h = () => H() % 2 || 12;
+    const hh = () => fillZero(h());
+    const m = () => date.getMinutes();
+    const mm = () => fillZero(m());
+    const s = () => date.getSeconds();
+    const ss = () => fillZero(s());
+    const SSS = () => date.getMilliseconds(); // 毫秒（[0...999]）
+    const SS = () => Math.floor(SSS() / 10); // 毫秒值 ([0...99])
+    const S = () => Math.floor(SS() / 10); // 毫秒值([0...9])
   };
 
   const format = (formatString: string) => {
