82d4a4556a43743f512266c1958be6243b61dc85
yanlele <331393627@qq.com>
2021-09-29 23:44:37 +0800

    feat: 计算节点貌似有点儿问题， 明天早上再来看看呗

--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/01、解法1.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/01、解法1.ts
@@ -2,15 +2,40 @@ import { ListNode } from '../../other/01、链表';
 import { head } from './data';
 
 function removeNthFromEnd(head: ListNode | null, n: number) {
-  let prevNode: ListNode, last: ListNode;
+  let prevNode: ListNode, nextNode: ListNode, currentNode: ListNode;
+  let node;
+  let length = 0;
   if (head === null) return;
-  prevNode = head;
-  last = head;
-  while (last && last.next !== null) {
-    last = last.next;
+  node = head;
+  let index = 0;
+  length++;
+  while (node && node.next !== null) {
+    node = node.next;
+    length++;
   }
-  console.log('last: ', last);
+
+  const currentIndex = length - n;
+  node = head;
+  while (node && node.next !== null) {
+    index++;
+    if (currentIndex === index) {
+      currentNode = node;
+    } else if (index - currentIndex === -1) {
+      prevNode = node;
+    }
+
+    node = node.next;
+  }
+
+  // 删除节点
+  if (currentNode === head) return head.next;
+
+  if (currentNode?.next) prevNode.next = currentNode.next;
+  if (!currentNode?.next) prevNode.next = null;
+
+  return head;
 }
 
-
-removeNthFromEnd(head, 1);
+// 算错了一位
+const result = removeNthFromEnd(head, 2);
+console.log(JSON.stringify(result, undefined, 4))
