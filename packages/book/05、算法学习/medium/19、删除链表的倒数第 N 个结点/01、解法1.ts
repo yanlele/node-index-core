a9c3574c60ecc41647d5c69607b1c027a6927bee
yanlele <331393627@qq.com>
2021-10-01 23:02:03 +0800

    feat: 防止错误

--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/01、解法1.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/01、解法1.ts
@@ -2,7 +2,7 @@ import { ListNode } from '../../other/01、链表';
 import { head } from './data';
 
 function removeNthFromEnd(head: ListNode | null, n: number) {
-  let prevNode: ListNode, nextNode: ListNode, currentNode: ListNode;
+  let prevNode: ListNode, currentNode: ListNode;
   let node;
   let length = 0;
   if (head === null) return;
@@ -31,11 +31,11 @@ function removeNthFromEnd(head: ListNode | null, n: number) {
   if (currentNode === head) return head.next;
 
   if (currentNode?.next) prevNode.next = currentNode.next;
-  if (!currentNode?.next) prevNode.next = null;
+  if (!currentNode?.next && prevNode?.next) prevNode.next = null;
 
   return head;
 }
 
 // 算错了一位
-const result = removeNthFromEnd(head, 2);
-console.log(JSON.stringify(result, undefined, 4))
+const result = removeNthFromEnd(head, 1);
+console.log(JSON.stringify(result, undefined, 4));
--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/data.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/data.ts
@@ -1,6 +1,6 @@
 import { NodeList } from '../../other/01、链表';
 
-const arr = [1, 2, 3, 4, 5];
+const arr = [1];
 const nodeList = new NodeList();
 
 arr.forEach(item => nodeList.append(item));
