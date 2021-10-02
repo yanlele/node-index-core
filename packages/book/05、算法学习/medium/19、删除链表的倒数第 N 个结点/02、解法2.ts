386637db5206f4e7a24f90bbc282f247dfa87f28
yanlele <331393627@qq.com>
2021-10-02 13:08:55 +0800

    feat: 双指针方法

--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/02、解法2.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/02、解法2.ts
@@ -1,7 +1,20 @@
 import { ListNode } from '../../other/01、链表';
 import { head } from './data';
 
+/**
+ * 双指针方法
+ * @param head
+ * @param n
+ */
 function removeNthFromEnd(head: ListNode | null, n: number) {
+  let fast = head, slow = head;
+  while (fast && n > 0) {
+    [fast, n] = [fast.next, n - 1];
+  }
 
+  while (fast) {
+    [fast, slow] = [fast.next, slow.next];
+  }
 
+  return slow;
 }
