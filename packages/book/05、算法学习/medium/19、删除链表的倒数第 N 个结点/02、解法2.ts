1e43738b0d90d25a7b5e8cf13ae45ea0ffe90ff0
yanlele <331393627@qq.com>
2021-10-02 22:02:06 +0800

    feat: 双向指针解法

--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/02、解法2.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/02、解法2.ts
@@ -3,18 +3,30 @@ import { head } from './data';
 
 /**
  * 双指针方法
+ *
+ * 本质上来说是一种镜像的解法
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/solution/shan-chu-lian-biao-de-dao-shu-di-nge-jie-ei10/
+ *
  * @param head
  * @param n
  */
 function removeNthFromEnd(head: ListNode | null, n: number) {
   let fast = head, slow = head;
+
   while (fast && n > 0) {
     [fast, n] = [fast.next, n - 1];
   }
 
-  while (fast) {
+  // 删除的是第一个元素
+  if (!fast) return head.next;
+
+  while (fast && fast.next !== null) {
     [fast, slow] = [fast.next, slow.next];
   }
 
-  return slow;
+  slow.next = slow.next.next;
+  return head;
 }
+
+const result = removeNthFromEnd(head, 2);
+console.log('result', JSON.stringify(result, undefined, 4))
--- packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/data.ts
+++ packages/book/05、算法学习/medium/19、删除链表的倒数第 N 个结点/data.ts
@@ -1,6 +1,6 @@
 import { NodeList } from '../../other/01、链表';
 
-const arr = [1];
+const arr = [1, 2];
 const nodeList = new NodeList();
 
 arr.forEach(item => nodeList.append(item));
